b2c027d4e68efb2d05811cc1d02709d88ec49525
Andy Yong <andyywz@gmail.com>
2013-07-05 09:57:22 -0400

    First commit.

---
+++ lib/active_record_lite/mass_object.rb
@@ -0,0 +1,13 @@
+class MassObject
+  def self.set_attrs(*attributes)
+  end
+
+  def self.attributes
+  end
+
+  def self.parse_all(results)
+  end
+
+  def initialize(params = {})
+  end
+end
---
+++ test/sql_object.rb
@@ -0,0 +1,27 @@
+require 'active_record_lite'
+
+# https://tomafro.net/2010/01/tip-relative-paths-with-file-expand-path
+cats_db_file_name =
+  File.expand_path(File.join(File.dirname(__FILE__), "cats.db"))
+DBConnection.open(cats_db_file_name)
+
+class Cat < SQLObject
+  set_table_name("cats")
+  set_attrs(:id, :name, :owner_id)
+end
+
+class Human < SQLObject
+  set_table_name("humans")
+  set_attrs(:id, :fname, :lname, :house_id)
+end
+
+p Human.find(1)
+p Cat.find(1)
+p Cat.find(2)
+
+p Human.all
+p Cat.all
+
+c = Cat.new(:name => "Gizmo", :owner_id => 1)
+c.save # create
+c.save # update
